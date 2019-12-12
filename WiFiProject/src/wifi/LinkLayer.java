47a7bd25a5bd7a06d7dda6459a618d003f9864f7
njchusid <nchusid@pugetsound.edu>
2019-12-11 20:36:38 -0800

    output stuff

--- WiFiProject/src/wifi/LinkLayer.java
+++ WiFiProject/src/wifi/LinkLayer.java
@@ -157,16 +157,18 @@ public class LinkLayer implements Dot11Interface
 				int prevDebug = this.debug;
 				this.debug = value;
 				output.println("Setting debug to " + value);
+				sender.setDebug(debug);
 				return prevDebug;
 			}
 			case 2: {
-				/*if (value != 0) {
+				if (value != 0) {
 					this.maxCollisionWindow = true;
+					sender.setCollisionWindow(value);
 				}
 				else {
 					this.maxCollisionWindow = false;
-				}*/
-				setCollisionWindow(value);
+					sender.setCollisionWindow(value);
+				}
 				if (this.maxCollisionWindow) {
 					System.out.println("Using the maximum Collision Window value each time");
 					return 0;
@@ -187,13 +189,5 @@ public class LinkLayer implements Dot11Interface
 		}
 		System.out.println("Unknown command: " + cmnd);
 		return 0;
-	}
-	
-	public synchronized void setCollisionWindow(int max) {
-		if (max != 0) {
-			maxCollisionWindow = true;
-		}else {
-			maxCollisionWindow = false;
-		}
 	}
 }
